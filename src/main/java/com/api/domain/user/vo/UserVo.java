ff05674969f3fd71543d6c2ea95df20eb28356c6
seoyamin <seoyamin@ewhain.net>
2025-02-20 13:55:49 +0900

    [fix] User addr, bday 제거

--- src/main/java/com/api/domain/user/vo/UserVo.java
+++ src/main/java/com/api/domain/user/vo/UserVo.java
@@ -2,16 +2,12 @@ package com.api.domain.user.vo;
 
 import lombok.Builder;
 
-import java.time.LocalDateTime;
-
 @Builder
 public record UserVo(
         Long id,
         String name,
         String email,
         String phone,
-        String addr,
-        LocalDateTime bday,
         String ceoNum,
         String company,
         String profileImg
