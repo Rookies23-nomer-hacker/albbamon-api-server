e0ac12199216bb5b578ce666f5bcb6a01ddd2583
seoyamin <seoyamin@ewhain.net>
2025-02-25 13:57:07 +0900

    [fix] RecruitmentRepository Param 어노테이션 추가

--- src/main/java/com/api/domain/recruitment/repository/RecruitmentRepository.java
+++ src/main/java/com/api/domain/recruitment/repository/RecruitmentRepository.java
@@ -5,6 +5,7 @@ import com.api.domain.recruitment.vo.RecruitmentDetailVo;
 import com.api.domain.recruitment.vo.RecruitmentVo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,11 +24,11 @@ public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> 
             "JOIN User u ON r.user = u " +
             "WHERE u.id = :userId " +
             "ORDER BY r.createDate desc")
-    List<RecruitmentVo> findAllRecruitmentVosByUserId(Long userId);
+    List<RecruitmentVo> findAllRecruitmentVosByUserId(@Param("userId") Long userId);
 
     @Query(value = "SELECT new com.api.domain.recruitment.vo.RecruitmentDetailVo(r.id, r.title, r.dueDate, r.contents, r.wage, u.name) " +
             "FROM Recruitment r " +
             "JOIN User u ON r.user = u " +
             "WHERE r.id = :recruitmentId")
-    Optional<RecruitmentDetailVo> findRecruitmentDetailVoById(Long recruitmentId);
+    Optional<RecruitmentDetailVo> findRecruitmentDetailVoById(@Param("recruitmentId") Long recruitmentId);
 }
