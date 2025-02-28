2df2b14c3dccfada424cd3ba9d68501f0e796c0d
satoshicingu <satoshichingu@gmail.com>
2025-02-28 13:00:41 +0900

    서버 적용

--- src/main/java/com/api/ApiApplication.java
+++ src/main/java/com/api/ApiApplication.java
@@ -2,14 +2,22 @@ package com.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
-public class ApiApplication {
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class ApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
+	
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ApiApplication.class);
+    }
 
 }
