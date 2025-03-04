810af091dc82040987ba363c980ff378589fe8df
seoyamin <seoyamin@ewhain.net>
2025-03-04 09:40:15 +0900

    [feat] Recruitment Pageable 처리

--- src/main/java/com/api/domain/recruitment/mapper/RecruitmentMapper.java
+++ src/main/java/com/api/domain/recruitment/mapper/RecruitmentMapper.java
@@ -2,13 +2,12 @@ package com.api.domain.recruitment.mapper;
 
 import com.api.domain.recruitment.dto.response.GetRecruitmentResponseDto;
 import com.api.domain.recruitment.vo.RecruitmentVo;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
 
 @Component
 public class RecruitmentMapper {
-    public GetRecruitmentResponseDto toGetRecruitmentResponseDto(List<RecruitmentVo> recruitmentList) {
+    public GetRecruitmentResponseDto toGetRecruitmentResponseDto(Page<RecruitmentVo> recruitmentList) {
         return GetRecruitmentResponseDto.of(recruitmentList);
     }
 }
--- src/main/java/com/api/domain/recruitment/repository/RecruitmentRepository.java
+++ src/main/java/com/api/domain/recruitment/repository/RecruitmentRepository.java
@@ -3,6 +3,8 @@ package com.api.domain.recruitment.repository;
 import com.api.domain.recruitment.entity.Recruitment;
 import com.api.domain.recruitment.vo.RecruitmentDetailVo;
 import com.api.domain.recruitment.vo.RecruitmentVo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,14 +19,14 @@ public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> 
             "FROM Recruitment r " +
             "LEFT JOIN User u ON r.user = u " +
             "ORDER BY r.createDate desc")
-    List<RecruitmentVo> findAllRecruitmentVos();
+    Page<RecruitmentVo> findAllRecruitmentVos(Pageable pageable);
 
     @Query(value = "SELECT new com.api.domain.recruitment.vo.RecruitmentVo(r.id, r.title, r.dueDate, r.createDate, r.wage, r.file, u.company, u.name, u.item) " +
             "FROM Recruitment r " +
             "JOIN User u ON r.user = u " +
             "WHERE u.id = :userId " +
             "ORDER BY r.createDate desc")
-    List<RecruitmentVo> findAllRecruitmentVosByUserId(@Param("userId") Long userId);
+    Page<RecruitmentVo> findAllRecruitmentVosByUserId(@Param("userId") Long userId, Pageable pageable);
 
     @Query(value = "SELECT new com.api.domain.recruitment.vo.RecruitmentDetailVo(r.id, r.title, r.dueDate, r.createDate, r.contents, r.wage, u.name, u.company, r.file) " +
             "FROM Recruitment r " +
