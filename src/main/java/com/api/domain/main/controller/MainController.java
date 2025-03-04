101ecb9434bbd7d0bcf77781830b45e37f977e79
seoyamin <seoyamin@ewhain.net>
2025-03-04 10:55:12 +0900

    [chore] MainController 수정

--- src/main/java/com/api/domain/main/controller/MainController.java
+++ src/main/java/com/api/domain/main/controller/MainController.java
@@ -1,27 +1,9 @@
 package com.api.domain.main.controller;
 
-import java.text.NumberFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.api.domain.main.service.MainService;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -29,13 +11,9 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/")
 public class MainController {
 	
-	private final MainService mainService;
-	
 	@GetMapping("/")
 	public String rootUrl() {
 	    return "알빠몬 API 서버(60085)";
 	}
-	
-	
 
 }
